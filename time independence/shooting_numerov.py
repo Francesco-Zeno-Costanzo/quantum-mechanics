7c242eff2d2164fbe6cbbba081b89d3cfb7649e9
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2023-06-15 14:12:45 +0200

    docu and comment

--- time independence/shooting_numerov.py
+++ time independence/shooting_numerov.py
@@ -10,7 +10,21 @@ import matplotlib.pyplot as plt
 def V(x, x0, V0, l):
     '''
     potential
-    x : position
+    
+    Parameters
+    ----------
+    x : float
+        position
+    x0 : float
+        position of well
+    V0 : float
+        value of well
+    l : float
+        angular momentum
+    
+    Returns
+    -------
+    potential of system
     '''
     V = 0
     if x > x0 or x == 0:
@@ -18,96 +32,162 @@ def V(x, x0, V0, l):
     elif x < x0:
         V = V0
   
-    return V - l*(l+1)/x**2
+    return V - 2*l*(l+1)/x**2
 
 
 def F(x, E, x0, V0, l) :
+    '''
+    Function f in u'' + f*u = 0
     
-    dydt = 2*(V(x, x0, V0, l) - E)
+    Parameters
+    ----------
+    x : float
+        position
+    E : float
+        energy of the system
+    x0 : float
+        position of well
+    V0 : float
+        value of well
+    l : float
+        angular momentum
     
-    return dydt
+    Returns
+    -------
+    value of function
+    '''
+    
+    f = 2*(V(x, x0, V0, l) - E)
+    
+    return f
+
 
 def Numerov(num_steps, xi, xf, init_f, init_b, f, args=()):
-    """
-    Integrator with Adams-Bashforth-Moulton
-    predictor and corretor of order 4
+    '''
+    Integrator with Numerov method modified for shooting.
+    The integration is compute foward and backward and
+    then we impose the continuity of fanction and compute
+    the jump of the first derivative.
+    The match of the two solution, inward and outward
+    is done in the classical turning point.
 
     Parameters
     ----------
     num_steps : int
         number of point of solution
-    tf : float
+    xi : float
+        lower bound of integration
+    xf : float
         upper bound of integration
+    init_f : 1darray
+        array of initial condition for foward integration
+    init_b : 1darray
+        array of initial condition for backward integration
     f : callable
         function to integrate, must accept vectorial input
-    init : 1darray
-        array of initial condition
     args : tuple, optional
         extra arguments to pass to f
 
     Return
     ------
-    X : array, shape (num_steps + 1, len(init))
+    x : 1darray
+        position
+    y : 1darray
         solution of equation
-    t : 1darray
-        time
-    """
-    #time steps
-    dx = (xf - xi)/num_steps
-
-    y = np.zeros(num_steps + 1)    #matrice delle soluzioni
-
-    y[0],  y[1]  = init_f              #condizioni iniziali
-    y[-1], y[-2] = init_b              #condizioni iniziali
+    jump : float
+        jump of derivative
+    '''
     
-    x     = np.array([xi + i*dx for i in range(num_steps + 1)])
+    dx = (xf - xi)/num_steps     # integration step
+
+    y = np.zeros(num_steps + 1)  # array of solution
+
+    y[0],  y[1]  = init_f        # initial condition for foward integration
+    y[-1], y[-2] = init_b        # initial condition for backward integration
+    
+    x     = np.array([xi + i*dx for i in range(num_steps + 1)])  # array of posizion
     fact  = np.array([dx**2 / 12 * f(z, *args) for z in x])
     index = 0
     for i in range(num_steps):
-        if fact[i] != np.copysign(fact[i], fact[i+1]):
-            index = i
+        if fact[i] != np.copysign(fact[i], fact[i+1]):           # search of classical turning point
+            index = i # index of classical turning point
     
-    fact = 1 - fact
+    fact = 1 - fact  # usefull quantity in integration
     
-    for i in range(1, index):
+    for i in range(1, index): # foward integration
         num = (12 - 10*fact[i])*y[i] - fact[i - 1]*y[i - 1]
         den = fact[i + 1]
         y[i + 1] = num/den
     
     y_m = y[index]
     
-    for i in range(num_steps - 1, index, -1):
+    for i in range(num_steps - 1, index, -1): # backward integration
         num = (12 - 10*fact[i])*y[i] - fact[i + 1] * y[i + 1]
         den = fact[i - 1]
         y[i - 1] = num/den
     
-    y_m /= y[index]
+    y_m /= y[index]   # scaling factor for continuity of function
     
     for i in range(index, num_steps + 1):
-        y[i] *= y_m
+        y[i] *= y_m   # impose continuiti
     
-    y = y/np.sqrt(si.simps(np.square(y), x, even='first'))
+    y = y/np.sqrt(si.simps(np.square(y), x, even='first')) # normalization of function
     
     jump = (y[index + 1] + y[index - 1] - (14 - 12*fact[i]) * y[index]) / dx
-    jump = jump * y[index]
+    jump = jump * y[index] # jump of first derivative
 
     return x, y, jump
 
 
-def shooting(num_steps, x_min, x_max, init_guess, step, tol, args=()):
+def shooting(num_steps, x_min, x_max, init_guess, step, tol, f, args=()):
+    '''
+    Shooting algorithm for finding eigenvalue and psi
+    via bisection method
+    
+    Parameters
+    ----------
+    num_steps : int
+        number of point of solution
+    x_min : float
+        lower bound of integration
+    x_max : float
+        upper bound of integration
+    init_guess : float
+        initial guess for energy
+    step : float
+        step for searching of second bound
+    tol : float
+        tollerance for bisection
+    f : callable
+        function to integrate, must accept vectorial input
+    args : tuple, optional
+        extra arguments to pass to f
+
+    Return
+    ------
+    xm : 1darray
+        position
+    ym : 1darray
+        solution of equation
+    Em : float
+        eigenvalue
+    jumpm : float
+        jump of derivative
+    '''
     
     dx = (x_max - x_min)/num_steps
     E1 = init_guess
-    
+    # iniitial condition
     init_f = np.array([0, dx])
     init_b = np.array([np.exp(E1*x_max), np.exp(E1*(num_steps-1)*dx)])
     
-    x1, y1, jump1 = Numerov(num_steps, x_min, x_max, init_f, init_b, F, args=(E1, *args))
+    # first biund
+    x1, y1, jump1 = Numerov(num_steps, x_min, x_max, init_f, init_b, f, args=(E1, *args))
     print(f'bound_1 : jump = {jump1:.5f}, ene = {E1:.5f}')
-    
+    #second bound
     while True:
         E2 = E1 + step
-        x2, y2, jump2 = Numerov(num_steps, x_min, x_max, init_f, init_b, F, args=(E2, *args))
+        x2, y2, jump2 = Numerov(num_steps, x_min, x_max, init_f, init_b, f, args=(E2, *args))
 
         if (jump1 * jump2) < 0.0:
             break
@@ -115,10 +195,11 @@ def shooting(num_steps, x_min, x_max, init_guess, step, tol, args=()):
         E1 = E2
     print(f'bound_2 : jump = {jump2:.5f}, ene = {E2:.5f}')
     
+    # bisection
     while True:
         
         Em = (E1 + E2)/2.0     
-        xm, ym, jumpm = Numerov(num_steps, x_min, x_max, init_f, init_b, F, args=(Em, *args))
+        xm, ym, jumpm = Numerov(num_steps, x_min, x_max, init_f, init_b, f, args=(Em, *args))
         
         if (jumpm * jump1) > 0 :
             jump1 = jumpm
@@ -141,7 +222,7 @@ def shooting(num_steps, x_min, x_max, init_guess, step, tol, args=()):
 #================================================================================
 
 
-x_max = 30       # max distance
+x_max = 20       # max distance
 x_min = 1e-10    # min distance to avoid divergence in l*(l+1)/x**2
 steps = 3000     # Number of point
 step  = 0.001    # step for searching of second bound
@@ -158,7 +239,7 @@ l     = 0        # angular momentum
 
 start = time.time()
 
-x, psi, eigv, jump = shooting(steps, x_min, x_max, E1, step, tau, args=(x_b, V0, l))
+x, psi, eigv, jump = shooting(steps, x_min, x_max, E1, step, tau, F, args=(x_b, V0, l))
 
 end = time.time() - start
 
@@ -182,16 +263,3 @@ plt.ylabel('$\psi(x)$', fontsize=15)
 plt.xlabel('x', fontsize=15)
 
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
